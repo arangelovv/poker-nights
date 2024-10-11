8a3ccebfe5a835aca07272c5fc279647125b8920
rangelovvjobs <rangelovvjobs@gmail.com>
2024-10-11 13:07:07 +0300

    initial server setup

---
+++ poker-nights-server/main.ts
@@ -0,0 +1,10 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id); });
